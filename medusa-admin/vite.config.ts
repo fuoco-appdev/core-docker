5a82ca92c40e3e4cf4dac3b3d09595554df8305d
lucasfuoco <lucasfuoco@gmail.com>
2023-02-20 13:03:55 -0500

    Fix vite env

--- medusa-admin/vite.config.ts
+++ medusa-admin/vite.config.ts
@@ -1,40 +1,38 @@
 import path from "path"
 import dns from "dns"
-import { defineConfig, loadEnv } from "vite"
+import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+const dotenv = require("dotenv")
+const env = dotenv.config().parsed
 
 // Resolve localhost for Node v16 and older.
 // @see https://vitejs.dev/config/server-options.html#server-host.
 dns.setDefaultResultOrder("verbatim")
 
-export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), "")
-  console.log(env)
-  return {
-    plugins: [react()],
-    // Backwards-compat with Gatsby.
-    publicDir: "static",
-    server: {
-      port: 7000,
-      host: true,
+export default defineConfig({
+  plugins: [react()],
+  // Backwards-compat with Gatsby.
+  publicDir: "static",
+  server: {
+    port: 7000,
+    host: true,
+  },
+  define: {
+    __MEDUSA_BACKEND_URL__: env.MEDUSA_BACKEND_URL,
+  },
+  build: {
+    outDir: "public",
+  },
+  resolve: {
+    alias: {
+      gatsby: path.resolve(__dirname, "src/compat/gatsby-compat.tsx"),
+      "@reach/router": path.resolve(
+        __dirname,
+        "src/compat/reach-router-compat.tsx"
+      ),
     },
-    define: {
-      __MEDUSA_BACKEND_URL__: JSON.stringify(env.MEDUSA_BACKEND_URL),
-    },
-    build: {
-      outDir: "public",
-    },
-    resolve: {
-      alias: {
-        gatsby: path.resolve(__dirname, "src/compat/gatsby-compat.tsx"),
-        "@reach/router": path.resolve(
-          __dirname,
-          "src/compat/reach-router-compat.tsx"
-        ),
-      },
-    },
-    optimizeDeps: {
-      exclude: ["typeorm", "medusa-interfaces"],
-    },
-  }
+  },
+  optimizeDeps: {
+    exclude: ["typeorm", "medusa-interfaces"],
+  },
 })
