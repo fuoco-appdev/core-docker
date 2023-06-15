007b2f16d8119e18537b052862031c8e81153774
Lucas Fuoco <fuoco.appdev@gmail.com>
2023-06-15 13:21:53 +0400

    Add medusa server as submodule

--- medusa/develop.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-#Run migrations to ensure the database is updated
-medusa migrations run
-
-node ./node_modules/@medusajs/medusa/dist/scripts/migrate-inventory-items.js
-
-#Start development environment
-medusa develop
