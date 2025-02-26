9fdb42c3b4e82e49b29a1bd8cad3872a202d66c8
Lucas Fuoco <fuoco.appdev@gmail.com>
2025-02-26 16:01:40 -0500

    Fix stream in nginx

--- docker/kubernetes/port_forward.sh
+++ docker/kubernetes/port_forward.sh
@@ -2,9 +2,11 @@ STUDIO_DEPLOYMENT_NAME=$(kubectl get deployments --no-headers=true | grep "^stud
 ANALYTICS_DEPLOYMENT_NAME=$(kubectl get deployments --no-headers=true | grep "^analytics" | awk '{print $1}' | head -n 1)
 MEDUSA_SERVER_DEPLOYMENT_NAME=$(kubectl get deployments --no-headers=true | grep "^medusa-server" | awk '{print $1}' | head -n 1)
 DASHBOARD_DEPLOYMENT_NAME=$(kubectl get deployments --no-headers=true | grep "^dashboard" | awk '{print $1}' | head -n 1)
+KONG_DEPLOYMENT_NAME=$(kubectl get deployments --no-headers=true | grep "^kong" | awk '{print $1}' | head -n 1)
 DB_DEPLOYMENT_NAME=$(kubectl get deployments --no-headers=true | grep "^db" | awk '{print $1}' | head -n 1)
 kubectl port-forward deployment/$STUDIO_DEPLOYMENT_NAME 3000:3000 &
 kubectl port-forward deployment/$ANALYTICS_DEPLOYMENT_NAME 4000:4000 &
 kubectl port-forward deployment/$MEDUSA_SERVER_DEPLOYMENT_NAME 9010:9010 &
 kubectl port-forward deployment/$DASHBOARD_DEPLOYMENT_NAME 8001:8001 &
+kubectl port-forward deployment/$KONG_DEPLOYMENT_NAME 8000:8000 &
 kubectl port-forward deployment/$DB_DEPLOYMENT_NAME 5432:5432 &
